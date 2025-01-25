a7d6c864492c6fbd319cba8627701b57bfe376a0
mashael-qq1 <meshael.qq1@gmail.com>
2025-01-25 19:39:42 +0300

    check

--- lib/main.dart
+++ lib/main.dart
@@ -123,7 +123,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Hello AutoCallers',
             ),
             Text(
               '$_counter',
