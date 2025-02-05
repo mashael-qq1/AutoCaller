23e38e467928dfc1070c519132a0cd63e6b5e7f8
mashael-qq1 <meshael.qq1@gmail.com>
2025-02-05 21:02:38 +0300

    edit homepage

--- lib/SchoolAdmin/AdminHomePage.dart
+++ lib/SchoolAdmin/AdminHomePage.dart
@@ -5,117 +5,128 @@ class SchoolAdminHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('School Admin Dashboard'),
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        centerTitle: true,
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Center(
-            child: ConstrainedBox(
-              constraints: BoxConstraints(
-                maxWidth: 1200, // Limits the width of the page for larger screens
-              ),
-              child: Column(
-                children: [
-                  const SizedBox(height: 20),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFFE3F2FD), Color(0xFF90CAF9)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Column(
+          children: [
+            AppBar(
+              title: const Text('School Admin Dashboard'),
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              centerTitle: true,
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Center(
+                    child: ConstrainedBox(
+                      constraints: BoxConstraints(
+                        maxWidth: 1200,
+                      ),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min, // Adjusts to child size
+                        children: [
+                          const SizedBox(height: 20),
 
-                  // Container to hold the welcome message and cards
-                  Container(
-                    padding: const EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(15),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.1),
-                          spreadRadius: 3,
-                          blurRadius: 10,
-                          offset: const Offset(0, 4),
-                        ),
-                      ],
-                    ),
-                    child: Column(
-                      children: [
-                        const Text(
-                          'Welcome to SchoolFlow, the place where you can manage everything with ease!',
-                          style: TextStyle(
-                            fontSize: 26,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                        const SizedBox(height: 30),
-
-                        const Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 20.0),
-                          child: Text(
-                            'Manage your school profile, students, and dismissal status effortlessly with SchoolFlow.',
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.w600,
-                              color: Colors.black54,
+                          // Container for welcome message and cards
+                          Container(
+                            padding: const EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(15),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black.withOpacity(0.1),
+                                  spreadRadius: 3,
+                                  blurRadius: 10,
+                                  offset: const Offset(0, 4),
+                                ),
+                              ],
                             ),
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                        const SizedBox(height: 30),
+                            child: Column(
+                              children: [
+                                const Text(
+                                  'Welcome to SchoolFlow, the place where you can manage everything with ease!',
+                                  style: TextStyle(
+                                    fontSize: 26,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                                const SizedBox(height: 30),
 
-                        // Horizontal scrolling for navigation cards
-                        SingleChildScrollView(
-                          scrollDirection: Axis.horizontal, // Horizontal scroll
-                          child: Row(
-                            children: [
-                              _createNavCard(
-                                context,
-                                'School Profile',
-                                Icons.school,
-                                Colors.blue.shade500, // Consistent blue shade
-                                () {
-                                  // Navigate to School Profile
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => SchoolProfilePage(),
+                                const Padding(
+                                  padding: EdgeInsets.symmetric(horizontal: 20.0),
+                                  child: Text(
+                                    'Manage your school profile, students, and dismissal status effortlessly with SchoolFlow.',
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w600,
+                                      color: Colors.black54,
                                     ),
-                                  );
-                                },
-                              ),
-                              const SizedBox(width: 20),
-                              _createNavCard(
-                                context,
-                                'Associated Students',
-                                Icons.people,
-                                Colors.blue.shade600, // Slightly darker blue
-                                () {
-                                  // Navigate to Associated Students
-                                },
-                              ),
-                              const SizedBox(width: 20),
-                              _createNavCard(
-                                context,
-                                'Dismissal Status',
-                                Icons.access_time,
-                                Colors.blue.shade700, // Even darker blue
-                                () {
-                                  // Navigate to Dismissal Status
-                                },
-                              ),
-                            ],
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                                const SizedBox(height: 30),
+
+                                // Horizontal scrolling for navigation cards
+                                SingleChildScrollView(
+                                  scrollDirection: Axis.horizontal,
+                                  child: Row(
+                                    children: [
+                                      _createNavCard(
+                                        context,
+                                        'School Profile',
+                                        Icons.school,
+                                        Colors.blue.shade500,
+                                        () {
+                                          Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                              builder: (context) => SchoolProfilePage(),
+                                            ),
+                                          );
+                                        },
+                                      ),
+                                      const SizedBox(width: 20),
+                                      _createNavCard(
+                                        context,
+                                        'Associated Students',
+                                        Icons.people,
+                                        Colors.blue.shade600,
+                                        () {},
+                                      ),
+                                      const SizedBox(width: 20),
+                                      _createNavCard(
+                                        context,
+                                        'Dismissal Status',
+                                        Icons.access_time,
+                                        Colors.blue.shade700,
+                                        () {},
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
-                        ),
-                      ],
+                          const SizedBox(height: 40),
+                        ],
+                      ),
                     ),
                   ),
-                  const SizedBox(height: 40),
-                ],
+                ),
               ),
             ),
-          ),
+          ],
         ),
       ),
     );
@@ -141,8 +152,8 @@ class SchoolAdminHomePage extends StatelessWidget {
           ],
         ),
         child: SizedBox(
-          width: 220, // Width of the card
-          height: 220, // Height of the card
+          width: 220,
+          height: 220,
           child: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -161,44 +172,6 @@ class SchoolAdminHomePage extends StatelessWidget {
               ],
             ),
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class SchoolProfilePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('School Profile'),
-        backgroundColor: Colors.blue.shade500,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Icon(
-              Icons.school,
-              size: 100,
-              color: Colors.blue,
-            ),
-            SizedBox(height: 20),
-            Text(
-              'School Profile Details Here',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-              ),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'You can edit or view your school\'s information here.',
-              style: TextStyle(fontSize: 18, color: Colors.black54),
-            ),
-          ],
         ),
       ),
     );
