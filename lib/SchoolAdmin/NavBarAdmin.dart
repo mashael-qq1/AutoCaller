0898f592d9954d051d4e69a4745c091804915eab
157nouraalhumaid <146860215+157nouraalhumaid@users.noreply.github.com>
2025-02-11 00:45:15 +0300

    admin nav without icons

--- lib/SchoolAdmin/NavBarAdmin.dart
+++ lib/SchoolAdmin/NavBarAdmin.dart
@@ -4,8 +4,6 @@ import 'AdminHomePage.dart';
 import 'DismissalStatus.dart';
 import 'StudentListAdmin.dart';
 import 'SchoolProfile.dart';
-// ignore: duplicate_import
-import 'package:autocaller/PrimaryGuardian/signup.dart';
 
 class NavBarAdmin extends StatelessWidget {
   final int currentIndex;
@@ -48,35 +46,23 @@ class NavBarAdmin extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          _buildNavItem(Icons.access_time, "Dismissal", 0, context),
-          _buildNavItem(Icons.person_add, "Add Guardian", 1, context),
-          _buildNavItem(Icons.home, "Home", 2, context),
-          _buildNavItem(Icons.group, "Students", 3, context),
-          _buildNavItem(Icons.account_circle, "Profile", 4, context),
+          _buildNavItem(Icons.access_time, 0, context),
+          _buildNavItem(Icons.person_add, 1, context),
+          _buildNavItem(Icons.home, 2, context),
+          _buildNavItem(Icons.group, 3, context),
+          _buildNavItem(Icons.account_circle, 4, context),
         ],
       ),
     );
   }
 
-  Widget _buildNavItem(
-      IconData icon, String label, int index, BuildContext context) {
+  Widget _buildNavItem(IconData icon, int index, BuildContext context) {
     return GestureDetector(
       onTap: () => _onItemTapped(context, index),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Icon(
-            icon,
-            color: currentIndex == index ? Colors.blue : Colors.grey,
-          ),
-          Text(
-            label,
-            style: TextStyle(
-              color: currentIndex == index ? Colors.blue : Colors.grey,
-              fontSize: 12,
-            ),
-          ),
-        ],
+      child: Icon(
+        icon,
+        color: currentIndex == index ? Colors.blue : Colors.grey,
+        size: 28, // Adjust size if needed
       ),
     );
   }
