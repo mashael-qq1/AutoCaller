e5f0fe431fb09f7441ef81a278d2aeaa7fdc9149
157nouraalhumaid <146860215+157nouraalhumaid@users.noreply.github.com>
2025-05-05 00:51:48 +0300

    Update ResetPassword.dart  sara

--- lib/SchoolAdmin/ResetPassword.dart
+++ lib/SchoolAdmin/ResetPassword.dart
@@ -61,7 +61,7 @@ class _ResetPasswordPageState extends State<ResetPasswordPage> {
         FocusScope.of(context).unfocus(); // Dismiss keyboard focus on tap
       },
       child: Scaffold(
-       backgroundColor: Colors.white
+       backgroundColor: Colors.white,
         appBar: AppBar(
           elevation: 0,
           backgroundColor: Colors.transparent,
